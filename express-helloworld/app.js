9cd2321080b7f6160439c7781af363ac144fe6c8
fernandorosique <131667427+fernandorosique@users.noreply.github.com>
2023-04-26 09:56:05 +0200

    Update app.js
    Cambio2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World  y más pruebas...Chulo Fernando!\n');
+  res.send('Hello World  y más pruebas...Chulo Fernando Miercoles 26!\n');
 });
 app.get('/Alex', function(req, res) {
   res.send('Hello ALex!\n');
